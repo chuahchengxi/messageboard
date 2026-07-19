c78a958f364401a3112e1e56fafaf44b0e5a8c35
chuahchengxi <boyboychuah@hotmail.com>
2026-07-19 21:28:22 +0800

    added ChatMessage, declaring what ChatMessage means and what it stores.

---
+++ messageboard/ChatMessage.swift
@@ -0,0 +1,21 @@
+//
+//  ChatMessage.swift
+//  messageboard
+//
+//  Created by cheng xi on 19/7/26.
+//
+import Foundation
+
+struct ChatMessage: Codable, Identifiable{
+    let id: UUID
+    let text: String
+    let content: String
+    let sender: String
+    init(id:UUID = UUID(),text:String,content:String, sender:String){
+        self.id = id
+        self.text = text
+        self.content = content
+        self.sender = sender
+    }
+    
+}
