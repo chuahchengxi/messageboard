8d303b87f8e0b83662d5324f3f9ffbdb8521b342
chuahchengxi <boyboychuah@hotmail.com>
2026-07-19 21:19:32 +0800

    Initial Commit

---
+++ messageboard/messageboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  messageboardApp.swift
+//  messageboard
+//
+//  Created by cheng xi on 19/7/26.
+//
+
+import SwiftUI
+
+@main
+struct messageboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
